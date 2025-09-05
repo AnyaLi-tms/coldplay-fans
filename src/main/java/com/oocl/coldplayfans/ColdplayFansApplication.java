76ac4dc8a717e1223da6a9df7821b53222269ed6
LIAN18 <lian18@oocl.com>
2025-09-05 14:33:30 +0800

    feat: init

---
+++ src/main/java/com/oocl/coldplayfans/ColdplayFansApplication.java
@@ -0,0 +1,13 @@
+package com.oocl.coldplayfans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColdplayFansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColdplayFansApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oocl/coldplayfans/ColdplayFansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oocl.coldplayfans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColdplayFansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
