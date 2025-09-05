4eebb7bff36cb9b2f7d98d17f6a5c54ba27175aa
GONGDE <derrick.gong@oocl.com>
2025-09-05 16:26:53 +0800

    feat: add cross origin

--- src/main/java/com/oocl/coldplayfans/controller/BannerController.java
+++ src/main/java/com/oocl/coldplayfans/controller/BannerController.java
@@ -2,6 +2,7 @@ package com.oocl.coldplayfans.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.oocl.coldplayfans.dao.Banner;
 import com.oocl.coldplayfans.service.BannerService;
-import org.springframework.web.bind.annotation.RequestParam;
  
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/banners")
+@CrossOrigin
 public class BannerController {
 
 
