5f7a9f300c35bab59f91c2bd6eb698315fc90d60
HEHE7 <hehe7@oocl.com>
2025-09-09 11:01:49 +0800

    refactor: 修改查询余票sql，增加limit，保证一票一码

--- src/main/java/com/oocl/coldplayfans/repository/JpaTicketRepository.java
+++ src/main/java/com/oocl/coldplayfans/repository/JpaTicketRepository.java
@@ -20,6 +20,6 @@ public interface JpaTicketRepository extends JpaRepository<Ticket, Integer> {
     );
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("SELECT t FROM Ticket t WHERE t.concertId = :concertId AND t.seatArea = :seatArea AND (t.idNumber IS NULL OR t.idNumber = '') ORDER BY t.id ASC")
+    @Query("SELECT t FROM Ticket t WHERE t.concertId = :concertId AND t.seatArea = :seatArea AND (t.idNumber IS NULL OR t.idNumber = '') ORDER BY t.id ASC limit :limit")
     List<Ticket> findEmptyIdNumberWithLock(@Param("concertId") Integer concertId, @Param("seatArea") String seatArea, @Param("limit") int limit);
 }
