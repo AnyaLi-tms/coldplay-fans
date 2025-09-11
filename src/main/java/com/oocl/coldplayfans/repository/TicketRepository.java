dccbb25053218fd0f0cb7ea4641387fc5cdfdbda
HEHE7 <hehe7@oocl.com>
2025-09-11 10:10:34 +0800

    refactor: TicketDbRepository继承接口

--- src/main/java/com/oocl/coldplayfans/repository/TicketRepository.java
+++ src/main/java/com/oocl/coldplayfans/repository/TicketRepository.java
@@ -7,4 +7,12 @@ import java.util.List;
 public interface TicketRepository {
 
     public List<Ticket> getTicketsByConcertId(Integer concertId);
+
+    public List<Ticket> findTicketByIdNumsAndConcertId(Integer concertId, List<String> idNums);
+
+    public List<Ticket> findEmptyIdNumberWithLock(Integer concertId, String seatArea, Integer count);
+
+    public List<Ticket> saveAll(List<Ticket> tickets);
+
+    public List<Ticket> getTicketByUserId(int userId);
 }
